f9eef1364d2801041320c8bc0ff16573db774037
José Miguel Suen <jmiguelsuen@gmail.com>
2018-07-02 14:39:50 -0300

    Fine tune for deployment

--- src/main/java/com/example/ObdServer2Application.java
+++ src/main/java/com/example/ObdServer2Application.java
@@ -1,14 +1,12 @@
-package com.example.demo;
+package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
-@Configuration
+@SpringBootApplication
 @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @ComponentScan ("com.example")
 
--- src/test/java/com/example/demo/tests/ObdServer2ApplicationTests.java
+++ src/test/java/com/example/demo/tests/ObdServer2ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.tests;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
